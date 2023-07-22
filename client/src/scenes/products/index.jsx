7af4e3469e432213bf349dbf53f74e4ea99f49af
Simoh13 <s.yelleschaouche@esi-sba.dz>
2023-07-22 14:38:03 +0100

    added some changes

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import { Box,Card,CardActions,CardContent,Collapse,Button,Typography,Rating,useTheme,useMediaQuery } from '@mui/material'
 import { useGetProductsQuery } from 'state/api'
+import Header from 'components/Header'
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
+
+  const {data, isLoading} = useGetProductsQuery();
+  const isNonMobile = useMediaQuery("(min-width: 1000px)");
+  console.log(data);
+
+  return <Box m="1.5rem 2.5rem">
+    <Header title="Products" subTitle="See your list of products"/>
+    {data || !isLoading ? (
+      <Box mt="20px" 
+      display="grid" 
+      gridTemplateColumns="repeat(4,minmax(0,1fr))" 
+      justifyContent="space-between"
+      rowGap="20px"
+      columnGap="1.33%"
+      sx={{"& > div":{gridColumn : isNonMobile ? undefined : "span 4"}}}
+      >
+        
+      </Box> ) :( <>Loading...</>)
+    }
+  </Box>
+    
+    
+  
 }
 
 export default Products
