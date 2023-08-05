0fa95a8eec6b97b802b9db2881ed17fc104e6486
Simoh13 <s.yelleschaouche@esi-sba.dz>
2023-08-05 21:22:29 +0100

    working on the front

---
+++ client/src/components/OverviewChart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ResponsiveLine } from '@nivo/line'
+import { useTheme } from '@mui/material'
+import { useGetSalesQuery } from 'state/api'
+import { useMemo } from 'react'
+
+const OverviewChart = ({isDashboard = false, view}) => {
+  return (
+    <div>OverviewChart</div>
+  )
+}
+
+export default OverviewChart
--- client/src/scenes/overview/index.jsx
+++ client/src/scenes/overview/index.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box,useTheme,FormControl,MenuItem,InputLabel,Select } from '@mui/material'
+import Header from 'components/Header'
+import OverviewChart from 'components/OverviewChart'
 
 const Overview = () => {
+  const [view,setView] = useState('units')
   return (
-    <div>Overview</div>
+    <Box m="1.5rem 2.5rem">
+      <Header title="OVERVIEW" subTitle="Overview of general revenue and profit"/>
+      <Box height="75vh">
+        <FormControl sx={{mt:"1rem"}}>
+          <InputLabel>View</InputLabel>
+          <Select value={view} label="View" onChange={(e)=> setView(e.target.value)}>
+            <MenuItem value="sales">Sales</MenuItem>
+            <MenuItem value="units">Units</MenuItem>
+          </Select>
+        </FormControl>
+        <OverviewChart view={view} />
+      </Box>
+    </Box>
   )
 }
 
