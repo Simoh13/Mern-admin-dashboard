a5c8fbea56e3eed52f95196fd0f0aba16cde0716
Simoh13 <s.yelleschaouche@esi-sba.dz>
2023-08-13 16:58:24 +0100

    finished performance page backend

--- server/controllers/management.js
+++ server/controllers/management.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import User from '../models/User.js'
+import Transaction from '../models/Transaction.js'
 
 
 export const getAdmins = async (req,res) =>{
@@ -8,5 +9,38 @@ export const getAdmins = async (req,res) =>{
         res.status(200).json(admins)
     } catch (error) {
         res.status(404).json({message: error.message})
+    }
+}
+
+export const getUserPerformance = async (req,res) =>{
+    try {
+        const { id } = req.params
+        const userWithStats = await User.aggregate([
+            { $match: {_id: new mongoose.Types.ObjectId(id)}},
+            {
+                $lookup:{
+                    from:"affiliatestats",
+                    localField:"_id",
+                    foreignField:"userId",
+                    as: "affliateStats"
+                }
+            },
+            {
+                $unwind: "$affiliateStats"
+            }
+        ])
+        const filteredSaleTransaction = saleTransactions.filter(
+            (transaction) => transaction != null
+        )
+
+        res.status(200).json({user:userWithStats[0],sales: filteredSaleTransaction})
+
+        const saleTransactions = await Promise.all(
+            userWithStats[0].affiliateStats.affiliateSales.map((id)=>{
+                return Transaction.findById(id)
+            })
+        )
+    } catch (error) {
+        res.status(404).json({message : error.message})
     }
 }
